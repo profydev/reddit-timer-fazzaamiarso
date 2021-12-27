6c06c1d7d5bbe795ac90832ce625793a8e8af618
fazzaamiarso <fazzaamiarso@gmail.com>
2021-12-28 01:50:09 +0700

    Fix Bug

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
