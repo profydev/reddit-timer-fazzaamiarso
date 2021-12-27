95a489a5f02b8b11fc5c637b99889164b85949fe
fazzaamiarso <fazzaamiarso@gmail.com>
2021-12-28 01:46:27 +0700

    Fix bug

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<div>App Placeholder</div>} />
-      <Route path="/search" element={<></>} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>App Placeholder</div>} />
+        <Route path="/search" element={<></>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
