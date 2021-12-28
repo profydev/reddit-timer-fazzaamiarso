f8ed7a77bd9aab96e31e199c99086805fec36411
Fazza Razaq Amiarso <86255053+fazzaamiarso@users.noreply.github.com>
2021-12-28 16:49:08 +0700

    Create app skeleton (#1)
    * Added /search and / routes
    
    * Reset CSS with normalize.css
    
    * Install bitter and montserrat via cdn in index.html
    
    * Added global style in globals.css
    
    * Add .prettierrc to match eslint rules
    
    * Move stylesheets import to index.js
    
    * Fix bug
    
    * Fix Bug
    
    * Change app title and meta description
    
    * Change app title and meta description
    
    * Remove Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>App Placeholder</div>} />
+        <Route path="/search" element={<></>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
