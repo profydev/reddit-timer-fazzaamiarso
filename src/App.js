b45cdf836c5b4cc0db750ffae8b0b8d1b536fca7
fazzaamiarso <fazzaamiarso@gmail.com>
2021-12-28 16:30:47 +0700

    Change app title and meta description

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<div>App Placeholder</div>} />
         <Route path="/search" element={<></>} />
