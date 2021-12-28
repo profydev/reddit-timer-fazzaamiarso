52d2813be6a42463c83a93bf829fddfc0e323ddc
fazzaamiarso <fazzaamiarso@gmail.com>
2021-12-28 16:42:06 +0700

    Remove Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
         <Route path="/" element={<div>App Placeholder</div>} />
         <Route path="/search" element={<></>} />
